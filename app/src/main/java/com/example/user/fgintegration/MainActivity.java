f38f14931a6a2f036fb6b5522a60d04fc1b7ecf8
listovivek <listovivek@gmail.com>
2018-01-10 01:27:09 -0800

    googleplus integration

--- app/src/main/java/com/example/user/fgintegration/MainActivity.java
+++ app/src/main/java/com/example/user/fgintegration/MainActivity.java
@@ -7,7 +7,10 @@ import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.example.user.fgintegration.googleplusintegration.GooglePlusActivity;
+import com.example.user.fgintegration.googleplusintegration.MyApplication;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -16,6 +19,15 @@ import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 
 import org.json.JSONObject;
 
@@ -23,7 +35,12 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener {
+
+
+    ////////////////////
+    //for facebook integration
 
     CallbackManager callbackManager;
 
@@ -33,6 +50,25 @@ public class MainActivity extends AppCompatActivity {
 
     //This tutorial get kasheskey through coding.
     //http://www.theappguruz.com/blog/android-facebook-integration-tutorial
+
+
+
+    /////////////
+    //for google+ integration
+
+    //https://inducesmile.com/android/android-google-plus-api-login-integration-example/
+
+   // https://developers.google.com/identity/sign-in/android/start-integrating
+
+    private static final String TAG = "GoogleSignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInOptions gso;
+    private GoogleApiClient mGoogleApiClient;
+    public static final String PROFILE_USER_ID = "USER_ID";
+    public static final String PROFILE_DISPLAY_NAME = "PROFILE_DISPLAY_NAME";
+    public static final String PROFILE_USER_EMAIL = "USER_PROFILE_EMAIL";
+    public static final String PROFILE_IMAGE_URL = "PROFILE_IMAGE_URL";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,12 +95,39 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        /////////////
+        //for google+ integration
+
+        gso = ((MyApplication) getApplication()).getGoogleSignInOptions();
+        mGoogleApiClient = ((MyApplication)
+                getApplication()).getGoogleApiClient(MainActivity.this, this);
+        SignInButton mSignInButton = (SignInButton)findViewById(R.id.google_sign_in_button);
+        assert mSignInButton != null;
+        mSignInButton.setSize(SignInButton.SIZE_STANDARD);
+        mSignInButton.setScopes(gso.getScopeArray());
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        /////////////
+        //for google+ integration
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+        ///////////////
     }
 
     private void getUserDetails(LoginResult loginResult) {
@@ -86,9 +149,8 @@ public class MainActivity extends AppCompatActivity {
         Bundle permission_param = new Bundle();
         permission_param.putString("fields", "id,name,email, " +
                 "picture.width(120).height(120)");
-                data_request.setParameters(permission_param);
+        data_request.setParameters(permission_param);
         data_request.executeAsync();
-
 
     }
 
@@ -103,5 +165,58 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         // Logs 'app deactivate' App Event.
         AppEventsLogger.deactivateApp(this);
+    }
+
+    /////////////
+    //for google+ integration
+
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount userAccount = result.getSignInAccount();
+            String userId = userAccount.getId();
+            String displayedUsername = userAccount.getDisplayName();
+            String userEmail = userAccount.getEmail();
+            String userProfilePhoto = userAccount.getPhotoUrl().toString();
+            Intent googleSignInIntent = new Intent(MainActivity.this,
+                    GooglePlusActivity.class);
+            googleSignInIntent.putExtra(PROFILE_USER_ID, userId);
+            googleSignInIntent.putExtra(PROFILE_DISPLAY_NAME, displayedUsername);
+            googleSignInIntent.putExtra(PROFILE_USER_EMAIL, userEmail);
+            googleSignInIntent.putExtra(PROFILE_IMAGE_URL, userProfilePhoto);
+            startActivity(googleSignInIntent);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> opr =
+                Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    //hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 }
---
+++ app/src/main/java/com/example/user/fgintegration/googleplusintegration/MyApplication.java
@@ -0,0 +1,37 @@
+package com.example.user.fgintegration.googleplusintegration;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by USER on 10-01-2018.
+ */
+
+public class MyApplication extends Application {
+
+    private GoogleApiClient mGoogleApiClient;
+    private GoogleSignInOptions gso;
+    public AppCompatActivity activity;
+
+
+    public GoogleSignInOptions getGoogleSignInOptions(){
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        return gso;
+    }
+    public GoogleApiClient getGoogleApiClient(AppCompatActivity activity,
+                                              GoogleApiClient.OnConnectionFailedListener
+                                                      listener){
+        this.activity = activity;
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this.activity, listener)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, getGoogleSignInOptions())
+                .build();
+        return mGoogleApiClient;
+    }
+}
